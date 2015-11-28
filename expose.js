9afba98e95d066f4e85ad43a2d28fb23dd3888d9
Elias Karakoulakis <elias.karakoulakis@gmail.com>
2015-11-28 10:31:47 +0200

    initial implementation of expose

---
+++ expose.js
@@ -0,0 +1,21 @@
+var binding = require( 'bindings' )( 'tcl' );
+var tcl     = new binding.TclBinding();
+
+tcl.expose(
+	function() { console.log('hello!') }, 
+	'consolelog0'
+);
+
+tcl.expose(
+	function(arg1) { console.log('hello ' + arg1 + '!'); }, 
+	'consolelog1'
+);
+
+tcl.expose(
+	function(n) { return n*n; }, 
+	'square'
+);
+
+tcl.cmdSync( "consolelog0" );
+tcl.cmdSync( "consolelog1 world" );
+tcl.cmdSync( 'puts "I think that 8*8 == [square 8]"' );
