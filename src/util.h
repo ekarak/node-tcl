314b104ffde5e518f407b2c72388a916f93920a0
Elias Karakoulakis <elias.karakoulakis@gmail.com>
2016-01-06 11:28:24 +0200

    improve logging

--- src/util.h
+++ src/util.h
@@ -23,6 +23,35 @@ v8::Local<v8::Value> TclToV8(Tcl_Interp*, Tcl_Obj*);
 Tcl_Obj* V8ToTcl(Tcl_Interp* interp, v8::Value* v8v);
 Tcl_Obj* V8ToTcl(Tcl_Interp* interp, v8::Local<v8::Value> v8v);
 
+// init new Isolate and ENTER it
 v8::Isolate* newV8Isolate();
+
+// generic logger
+void v8log(const char* format, ...);
+
+#include <unistd.h>
+#include <pthread.h>
+	class mutex {
+	private:
+		pthread_mutex_t _mutex;
+
+	public:
+		mutex()             { pthread_mutex_init(&_mutex, NULL); }
+		~mutex()            { pthread_mutex_destroy(&_mutex); }
+		inline void lock()  { pthread_mutex_lock(&_mutex); }
+		inline void unlock(){ pthread_mutex_unlock(&_mutex); }
+
+
+	class scoped_lock {
+		public:
+			inline explicit scoped_lock(mutex & sp) : _sl(sp) { _sl.lock(); }
+			inline ~scoped_lock()                             { _sl.unlock(); }
+		private:
+			scoped_lock(scoped_lock const &);
+			scoped_lock & operator=(scoped_lock const &);
+			mutex& _sl;
+		};
+
+	};
 
 #endif /* SRC_UTIL_H_ */
